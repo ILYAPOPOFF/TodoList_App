837ef35a7a1843f156dc4e744f484556b77080dc
ILYAPOPOFF <IlyaP1998@gmail.com>
2023-09-22 00:16:49 +0400

    Creating a List and Toolbar

---
+++ TodoList_App/Views/AddView.swift
@@ -0,0 +1,49 @@
+//
+//  AddView.swift
+//  TodoList_App
+//
+//  Created by ILYA POPOV on 22.09.2023.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
+                .cornerRadius(10)
+                
+                Button {
+                    
+                } label: {
+                    Text("Save")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 50)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                }
+
+            }
+            .padding(16)
+        }
+        .navigationTitle("Add an Item 🖊️")
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AddView()
+        }
+    }
+}
+
---
+++ TodoList_App/Views/ListView.swift
@@ -0,0 +1,47 @@
+//
+//  ListView.swift
+//  TodoList_App
+//
+//  Created by ILYA POPOV on 22.09.2023.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    @State var items: [String] = [
+        "This is the first title!",
+        "This is the second!",
+        "Third!"
+    ]
+    
+    var body: some View {
+        List {
+            ForEach(items, id: \.self) { item in
+                ListRowView(title: item)
+            }
+        }
+        .listStyle(.inset)
+        .navigationTitle("Todo List 📋")
+        //Toolbar
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                EditButton()
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                NavigationLink("Add") {
+                    AddView()
+                }
+            }
+        }
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ListView()
+        }
+    }
+}
+
