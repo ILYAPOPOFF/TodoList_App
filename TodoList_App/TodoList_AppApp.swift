d3fc15fe8fc8faaa9c3b678fa6015354113f0371
ILYAPOPOFF <IlyaP1998@gmail.com>
2023-09-22 00:02:14 +0400

    first commit

---
+++ TodoList_App/TodoList_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_AppApp.swift
+//  TodoList_App
+//
+//  Created by ILYA POPOV on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
